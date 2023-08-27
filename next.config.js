91e037cffd80eeb60bea5553517bb880b5406f07
akinloluwami <akinkunmioye42@gmail.com>
2023-08-27 11:49:13 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx'
+  })
+   
+  module.exports = withNextra({nextConfig})
+   
---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>Snaplog Docs</span>,
+  project: {
+    link: "https://github.com/akinloluwami/snaplog",
+  },
+  // ... other theme options
+};
