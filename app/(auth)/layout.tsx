0aa158c9cd570ce16acacd736ce948ec5637d822
akinloluwami <akinkunmioye42@gmail.com>
2023-08-28 02:23:43 +0100

    conditional rendering for text

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,25 @@
+"use client";
+
 import Logo from "@/components/Logo";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+
   return (
     <div className="flex h-screen">
       <div className="w-[40%] pt-7 pb-3 px-7 flex justify-between h-full flex-col">
         <div className="">
           <Logo />
           <div className="mt-8">
-            <h2 className="text-2xl font-semibold">Welcome back</h2>
+            <h2 className="text-2xl font-semibold">
+              {pathname === "/login" && "Welcome back"}
+              {pathname === "/signup" && "Get started"}
+            </h2>
             <small className="font-semibold text-gray-400">
-              Sign into your account
+              {pathname === "/login" && "Login to your account"}
+              {pathname === "/signup" && "Create a new account"}
             </small>
           </div>
           <div className="h-full mt-20 flex items-center justify-center">
