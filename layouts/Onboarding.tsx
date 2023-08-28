d2980aabadf8b965d012de3847fb1a9593825638
akinloluwami <akinkunmioye42@gmail.com>
2023-08-28 14:36:01 +0100

    import state

--- layouts/Onboarding.tsx
+++ layouts/Onboarding.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { usePathname } from "next/navigation";
+import useOnboardingStore from "@/stores/onboardingStore";
 
 const OnboardingLayout = ({ children }: { children: ReactNode }) => {
-  const steps = [
-    { title: "Get started", route: "/onboarding" },
-    { title: "Integrate", route: "/onboarding/integrate" },
-    { title: "Ready!", route: "/onboarding/ready" },
-  ];
+  const { steps, currentStepId } = useOnboardingStore();
+
   return (
     <div className="py-5">
       <h1 className="text-center text-3xl font-semibold mt-8">
@@ -15,8 +14,15 @@ const OnboardingLayout = ({ children }: { children: ReactNode }) => {
       <div className="flex items-center justify-center gap-20 mt-10">
         {steps.map((step, i) => (
           <Link
+            onClick={(e) => {
+              !step.isDone && step.id !== currentStepId && e.preventDefault();
+            }}
             href={step.route}
-            className="group flex items-center gap-2 font-semibold"
+            className={`group flex items-center gap-2 font-semibold ${
+              !step.isDone && step.id !== currentStepId
+                ? "opacity-60 cursor-not-allowed"
+                : "opacity-100"
+            }`}
             key={i}
           >
             <div className="bg-gradient-to-tl from-purple-800 flex items-center justify-center text-2xl to-purple-500 text-white rounded-full w-12 h-12 ">
