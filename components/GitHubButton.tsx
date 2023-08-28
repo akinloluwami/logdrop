e08d6ea24f02dd686adad46cf5f1254df6865434
akinloluwami <akinkunmioye42@gmail.com>
2023-08-28 04:11:43 +0100

    styled button

--- components/GitHubButton.tsx
+++ components/GitHubButton.tsx
@@ -2,7 +2,10 @@ import { SiGithub } from "react-icons/si";
 
 const GitHubButton = ({ action }: { action: () => void }) => {
   return (
-    <button onClick={action}>
+    <button
+      onClick={action}
+      className="flex items-center gap-3 bg-white/10 px-16 py-4 rounded-full font-semibold bg-gradient-to-r from-transparent to-purple-500/50  hover:to-purple-500 transition-all"
+    >
       <SiGithub />
       Continue with GitHub
     </button>
