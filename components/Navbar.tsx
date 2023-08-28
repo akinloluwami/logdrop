8333a35427656d2cc7bd012a2d1aeadc2dc7a093
akinloluwami <akinkunmioye42@gmail.com>
2023-08-28 01:35:06 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,26 @@ const Navbar = () => {
   return (
     <div className="flex justify-between px-14 py-4">
       <h2>Snaplog</h2>
-      <div className="flex gap-6">
-        <Link href={"/login"}>Docs</Link>
-        <Link href={"/login"}>Login</Link>
-        <Link href={"/login"}>Get started</Link>
+      <div className="flex gap-12 items-center font-semibold">
+        <Link
+          href={"/docs"}
+          className="hover:text-purple-500 transition-colors"
+        >
+          Docs
+        </Link>
+        <Link
+          href={"/login"}
+          className="hover:text-purple-500 transition-colors"
+        >
+          Login
+        </Link>
+
+        <Link
+          href={"/signup"}
+          className="bg-white px-4 py-2 rounded-full text-black hover:bg-gray-100 duration-300 ease-in-out border-2 border-transparent hover:border-purple-500 transition-colors"
+        >
+          Get started
+        </Link>
       </div>
     </div>
   );
