a99134f847f3472802417cdfbdacfe65b64da9f7
akinloluwami <akinkunmioye42@gmail.com>
2023-08-29 03:12:15 +0100

    handleNext

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -1,6 +1,24 @@
 import OnboardingLayout from "@/layouts/Onboarding";
+import useOnboardingStore from "@/stores/onboardingStore";
+import { useRouter } from "next/router";
 
 const Onboarding = () => {
+  const { steps, updateSteps, updateCurrentStep } = useOnboardingStore();
+
+  const router = useRouter();
+
+  const handleNextClick = () => {
+    const updatedSteps = [...steps];
+    updatedSteps[0].isDone = true;
+
+    const currentStepIndex = steps.findIndex((step) => step.id === steps[0].id);
+
+    if (currentStepIndex !== -1 && currentStepIndex + 1 < steps.length) {
+      updateCurrentStep(steps[currentStepIndex + 1].id);
+    }
+    updateSteps(updatedSteps);
+    router.push(steps[currentStepIndex + 1].route);
+  };
   return (
     <OnboardingLayout>
       <div className="">
@@ -8,7 +26,13 @@ const Onboarding = () => {
           Create your first project.
         </h3>
 
-        <form className="flex items-center flex-col gap-5 mt-6 w-[400px]">
+        <form
+          className="flex items-center flex-col gap-5 mt-6 w-[400px]"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleNextClick();
+          }}
+        >
           <input
             className="w-full rounded-full border border-white/10 py-3 pl-3 pr-5 bg-transparent outline-none focus:border-purple-500 transition-colors"
             type="text"
