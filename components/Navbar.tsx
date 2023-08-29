9fac0d6e8cffd49aa1f0e7595eb16ed4cbfa3f31
akinloluwami <akinkunmioye42@gmail.com>
2023-08-29 04:56:02 +0100

    highlight active link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,32 @@
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const links = [
+    { title: "Pricing", href: "/pricing" },
+    { title: "Docs", href: "/docs" },
+  ];
+
+  const pathname = usePathname();
+
   return (
     <div className="flex justify-between px-14 py-4">
       <Logo />
       <div className="flex gap-12 items-center font-semibold">
-        <Link
-          href={"/docs"}
-          className="hover:text-purple-500 transition-colors"
-        >
-          Docs
-        </Link>
+        {links.map((link, i) => (
+          <Link
+            key={i}
+            href={link.href}
+            className={`hover:text-purple-500 transition-colors ${
+              pathname === link.href && "text-purple-500"
+            }`}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </div>
+      <div className="flex gap-12 items-center font-semibold">
         <Link
           href={"/login"}
           className="hover:text-purple-500 transition-colors"
