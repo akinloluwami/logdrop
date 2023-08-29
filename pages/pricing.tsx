0a7ec3831fc2ee862fddacc0fd4d84f277f268d9
akinloluwami <akinkunmioye42@gmail.com>
2023-08-29 04:55:42 +0100

    initial commit

---
+++ pages/pricing.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Pricing;
