19fc1d6b6772f1b81549cae0025db9b89aba484c
akinloluwami <akinkunmioye42@gmail.com>
2023-08-31 10:35:53 +0100

    added page tabs

--- pages/dashboard/settings.tsx
+++ pages/dashboard/settings.tsx
@@ -1,7 +1,58 @@
 import DashboardLayout from "@/layouts/DashboardLayout";
+import {
+  ProgressBar,
+  Card,
+  Flex,
+  Text,
+  Metric,
+  TabList,
+  Tab,
+  TabGroup,
+  TabPanels,
+  TabPanel,
+} from "@tremor/react";
 
 const Settings = () => {
-  return <DashboardLayout pageTitle="Settings">Settings</DashboardLayout>;
+  return (
+    <DashboardLayout pageTitle="Settings">
+      <TabGroup>
+        <TabList
+          className="!bg-transparent border-gray-800"
+          variant="solid"
+          color="purple"
+        >
+          <Tab className="!bg-black">General</Tab>
+          <Tab className="!bg-black">Plan & Usage</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <div className="mt-10">
+              <Flex className="mt-4">
+                <Text className="w-full">Product Y</Text>
+                <Flex className="space-x-2" justifyContent="end">
+                  <Text>$ 108,799</Text>
+                  <Text>38%</Text>
+                </Flex>
+              </Flex>
+              <ProgressBar value={38} className="mt-2" />
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="mt-10">
+              <Flex className="mt-4">
+                <Text className="w-full">Product Z</Text>
+                <Flex className="space-x-2" justifyContent="end">
+                  <Text>$ 99,484</Text>
+                  <Text>16%</Text>
+                </Flex>
+              </Flex>
+              <ProgressBar value={12} className="mt-2" />
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </DashboardLayout>
+  );
 };
 
 export default Settings;
