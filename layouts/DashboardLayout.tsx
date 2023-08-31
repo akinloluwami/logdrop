0bbc6fae5bdd3a8abb778685792ab09cccbc1e7b
akinloluwami <akinkunmioye42@gmail.com>
2023-08-31 10:35:21 +0100

    changed border color

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -13,11 +13,11 @@ const DashboardLayout: FC<Props> = ({ children, pageTitle }) => {
       <Head>
         <title>{pageTitle} • Snaplog</title>
       </Head>
-      <div className="hidden lg:block border-r border-purple-500/20 w-[20%] fixed">
+      <div className="hidden lg:block border-r border-gray-800 w-[20%] fixed">
         <Sidebar />
       </div>
       <div className="w-[80%] ml-[20%]">
-        <div className="flex justify-between border-b border-purple-500/20 px-10 py-5 w-full sticky top-0 bg-black/20 backdrop-blur-md z-50">
+        <div className="flex justify-between border-b border-gray-800 px-10 py-5 w-full sticky top-0 bg-black/20 backdrop-blur-md z-50">
           <h2 className="text-2xl font-medium">{pageTitle}</h2>
           <button>Docs</button>
         </div>
