0fbb6181056f57f0ed39cd27a998a7164f4c411b
akinloluwami <akinkunmioye42@gmail.com>
2023-08-31 09:37:21 +0100

    added backdrop filter

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -8,11 +8,11 @@ interface Props {
 const DashboardLayout: FC<Props> = ({ children }) => {
   return (
     <div className="flex">
-      <div className="hidden lg:block border-r border-purple-500/20 w-[20%]">
+      <div className="hidden lg:block border-r border-purple-500/20 w-[20%] fixed">
         <Sidebar />
       </div>
-      <div className="w-[80%]">
-        <div className="flex justify-between border-b border-purple-500/20 px-7 py-5 w-full">
+      <div className="w-[80%] ml-[20%]">
+        <div className="flex justify-between border-b border-purple-500/20 px-7 py-5 w-full sticky top-0 bg-black/20 backdrop-blur-md z-50">
           <h2>Something goes here</h2>
           <button>Docs</button>
         </div>
