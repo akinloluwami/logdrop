7870c5ecc084d778ba093dc30ef057351a931985
akinloluwami <akinkunmioye42@gmail.com>
2023-08-31 10:16:30 +0100

    ..

--- pages/dashboard/requests.tsx
+++ pages/dashboard/requests.tsx
@@ -1,6 +1,35 @@
 import DashboardLayout from "@/layouts/DashboardLayout";
+import RequestsTable from "@/components/RequestsTable";
+
 const Requests = () => {
-  return <DashboardLayout pageTitle="Requests">Requests</DashboardLayout>;
+  const requests = [
+    {
+      endpoint: "/ping",
+      method: "GET",
+      status: 200,
+      timeTaken: "5ms",
+      createdAt: "25/08/2023",
+    },
+    {
+      endpoint: "/auth/signup",
+      method: "POST",
+      status: 201,
+      timeTaken: "5ms",
+      createdAt: "25/08/2023",
+    },
+    {
+      endpoint: "/auth/login",
+      method: "POST",
+      status: 400,
+      timeTaken: "5ms",
+      createdAt: "25/08/2023",
+    },
+  ];
+  return (
+    <DashboardLayout pageTitle="Requests">
+      <RequestsTable data={requests} />
+    </DashboardLayout>
+  );
 };
 
 export default Requests;
