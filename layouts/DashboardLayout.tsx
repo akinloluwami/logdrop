3f35fa1fc46f565703361a2a5b7ce475c20e5d92
akinloluwami <akinkunmioye42@gmail.com>
2023-08-31 04:10:19 +0100

    -r

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -8,7 +8,7 @@ interface Props {
 const DashboardLayout: FC<Props> = ({ children }) => {
   return (
     <div className="flex">
-      <div className="hidden lg:block border-r-2 border-purple-500/20 w-[20%]">
+      <div className="hidden lg:block border-r border-purple-500/20 w-[20%]">
         <Sidebar />
       </div>
       <div className="py-5 px-10">{children}</div>
