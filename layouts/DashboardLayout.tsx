98781b652389c193f2c3397ffd22a5d6e3f43a0f
akinloluwami <akinkunmioye42@gmail.com>
2023-08-31 04:14:48 +0100

    added new content and update styling

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -11,7 +11,13 @@ const DashboardLayout: FC<Props> = ({ children }) => {
       <div className="hidden lg:block border-r border-purple-500/20 w-[20%]">
         <Sidebar />
       </div>
-      <div className="py-5 px-10">{children}</div>
+      <div className="w-full">
+        <div className="flex justify-between border-b border-purple-500/20 px-7 py-5 w-full">
+          <h2>Something goes here</h2>
+          <button>Docs</button>
+        </div>
+        <div className="py-5 px-10">{children}</div>
+      </div>
     </div>
   );
 };
