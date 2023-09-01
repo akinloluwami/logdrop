a2e91dd5281db256700b57b628b7b31b8b12c5c6
akinloluwami <akinkunmioye42@gmail.com>
2023-09-01 23:10:27 +0100

    allowed more than one request

--- middlewares/requestMethod.ts
+++ middlewares/requestMethod.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-const requestMethod = (method: "GET" | "POST" | "DELETE" | "PATH") => {
+const requestMethod = (methods: string[]) => {
   return (handler: any) => (req: NextApiRequest, res: NextApiResponse) => {
-    if (method !== req.method) {
+    if (!methods.includes(req.method!)) {
       res.status(405).json({
         error: "Method Not Allowed",
-        message: `This endpoint only supports ${method}, while your request was: ${req.method}`,
+        message: `This endpoint only supports ${methods.join(
+          " ,"
+        )}, while your request was: ${req.method}`,
       });
       return;
     }
