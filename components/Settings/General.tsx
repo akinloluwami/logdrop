d51f593f499530440dfa63b5146d55c98288387b
akinloluwami <akinkunmioye42@gmail.com>
2023-09-01 17:49:03 +0100

    .

--- components/Settings/General.tsx
+++ components/Settings/General.tsx
@@ -2,15 +2,27 @@ import { Button, Flex, Text, TextInput } from "@tremor/react";
 
 const General = () => {
   return (
-    <div className="w-[450px]">
-      <p className="text-gray-300">Project name</p>
-      <Flex className="gap-2">
-        <TextInput
-          placeholder="Project name"
-          className="!bg-transparent my-2"
-        />
-        <Button color="purple">Save</Button>
-      </Flex>
+    <div className="w-[450px] flex flex-col gap-4">
+      <div className="">
+        <p className="text-gray-300">Project name</p>
+        <Flex className="gap-2">
+          <TextInput
+            placeholder="Project name"
+            className="!bg-transparent my-2"
+          />
+          <Button color="purple">Save</Button>
+        </Flex>
+      </div>
+      <div className="">
+        <p className="text-gray-300">API endpoint</p>
+        <Flex className="gap-2">
+          <TextInput
+            placeholder="API endpoint"
+            className="!bg-transparent my-2"
+          />
+          <Button color="purple">Save</Button>
+        </Flex>
+      </div>
     </div>
   );
 };
