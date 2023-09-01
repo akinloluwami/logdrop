2c2b56806e1fc00c2bc1f87e72186ae5d1b3cc81
akinloluwami <akinkunmioye42@gmail.com>
2023-09-01 20:27:09 +0100

    added page content

--- pages/api-keys.tsx
+++ pages/api-keys.tsx
@@ -1,6 +1,21 @@
+import ApiKeysTable from "@/components/ApiKeysTable";
 import DashboardLayout from "@/layouts/DashboardLayout";
 const ApiKeys = () => {
-  return <DashboardLayout pageTitle="API Keys">ApiKeys</DashboardLayout>;
+  const apiKeys = [
+    {
+      id: 1,
+      name: "key",
+      key: "bjo987ytf3vbntjkgovucytfvbnm",
+      timesUsed: 100,
+      lastUsed: "01/09/2023",
+      created: "28/08/2023",
+    },
+  ];
+  return (
+    <DashboardLayout pageTitle="API Keys">
+      <ApiKeysTable data={apiKeys} />
+    </DashboardLayout>
+  );
 };
 
 export default ApiKeys;
