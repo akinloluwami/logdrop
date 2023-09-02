8b40d1d251f20ea6e3a56476a4d7f0f261cbf6a1
akinloluwami <akinkunmioye42@gmail.com>
2023-09-02 14:21:18 +0100

    template

---
+++ pages/api/test.ts
@@ -0,0 +1,17 @@
+import { NextApiResponse } from "next";
+import { requestMethod } from "@/middlewares/requestMethod";
+import { CustomRequest } from "@/interfaces";
+import authenticateToken from "@/middlewares/auth";
+import { prisma } from "@/prisma";
+
+const handler = async (req: CustomRequest, res: NextApiResponse) => {
+  try {
+  } catch (error) {}
+};
+
+export default requestMethod(["GET"])((
+  req: CustomRequest,
+  res: NextApiResponse
+) => {
+  authenticateToken(req, res, () => handler(req, res));
+});
