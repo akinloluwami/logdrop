bfe3c46b78177c69f94859ca4c915dc5ad4f8274
akinloluwami <akinkunmioye42@gmail.com>
2023-09-02 19:33:26 +0100

    initial commit

---
+++ pages/api/onboarding/project.ts
@@ -0,0 +1,26 @@
+import { NextApiResponse } from "next";
+import { requestMethod } from "@/middlewares/requestMethod";
+import { CustomRequest } from "@/interfaces";
+import authenticateToken from "@/middlewares/auth";
+import { prisma } from "@/prisma";
+import project from "../project";
+
+const handler = async (req: CustomRequest, res: NextApiResponse) => {
+  try {
+    const project = await prisma.project.findFirst({
+      where: {
+        userId: req.user?.id,
+      },
+    });
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+export default requestMethod(["GET"])((
+  req: CustomRequest,
+  res: NextApiResponse
+) => {
+  authenticateToken(req, res, () => handler(req, res));
+});
