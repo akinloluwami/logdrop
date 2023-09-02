c204f768b0cfe5a308955ee737b40ab246af8b58
akinloluwami <akinkunmioye42@gmail.com>
2023-09-02 14:32:17 +0100

    create project

--- pages/api/project/index.ts
+++ pages/api/project/index.ts
@@ -1,9 +1,45 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiResponse } from "next";
 import { requestMethod } from "@/middlewares/requestMethod";
+import { CustomRequest } from "@/interfaces";
+import authenticateToken from "@/middlewares/auth";
+import { prisma } from "@/prisma";
+import validator from "validator";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-  }
+const handler = async (req: CustomRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === "POST") {
+      const { name, apiUrl } = req.body;
+
+      if (!name || !apiUrl) {
+        res.status(400).json({
+          message: "Project name and API URL are required",
+        });
+        return;
+      }
+
+      if (!validator.isURL(apiUrl, { require_protocol: true }))
+        res.status(400).json({
+          message: "Invalid API URL",
+        });
+
+      await prisma.project.create({
+        data: {
+          name,
+          apiUrl,
+          userId: req?.user?.id!,
+        },
+      });
+    }
+
+    res.status(201).json({
+      message: "Project created successfully",
+    });
+  } catch (error) {}
 };
 
-export default requestMethod(["POST", "GET"])(handler);
+export default requestMethod(["POST", "GET"])((
+  req: CustomRequest,
+  res: NextApiResponse
+) => {
+  authenticateToken(req, res, () => handler(req, res));
+});
