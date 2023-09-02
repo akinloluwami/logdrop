24e86ad4c69457e169a2dc330b94e68b4053476a
akinloluwami <akinkunmioye42@gmail.com>
2023-09-02 19:33:46 +0100

    update

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -1,13 +1,36 @@
+import { axios } from "@/configs/axios";
 import OnboardingLayout from "@/layouts/Onboarding";
 import useOnboardingStore from "@/stores/onboardingStore";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { CgSpinner } from "react-icons/cg";
 
 const Onboarding = () => {
   const { steps, updateSteps, updateCurrentStep } = useOnboardingStore();
 
+  const [name, setName] = useState("");
+  const [apiUrl, setApiUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [disabledInputs, setDisableInputs] = useState(false);
+
   const router = useRouter();
 
-  const handleNextClick = () => {
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios("/onboarding/project");
+        console.log(data);
+
+        if (data.id) {
+          setApiUrl(data.apiUrl);
+          setName(data.name);
+          setDisableInputs(true);
+        }
+      } catch (error) {}
+    })();
+  }, []);
+
+  const goToNextStep = () => {
     const updatedSteps = [...steps];
     updatedSteps[0].isDone = true;
 
@@ -18,6 +41,21 @@ const Onboarding = () => {
     }
     updateSteps(updatedSteps);
     router.push(steps[currentStepIndex + 1].route);
+  };
+
+  const handleNextClick = async () => {
+    if (disabledInputs) {
+      goToNextStep();
+      return;
+    }
+    setLoading(true);
+    try {
+      await axios.post("/project", { name, apiUrl });
+      goToNextStep();
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <OnboardingLayout>
@@ -34,17 +72,26 @@ const Onboarding = () => {
           }}
         >
           <input
-            className="w-full rounded-full border border-white/10 py-3 pl-3 pr-5 bg-transparent outline-none focus:border-purple-500 transition-colors"
+            className="w-full rounded-full border border-white/10 py-3 pl-3 pr-5 bg-transparent outline-none focus:border-purple-500 transition-colors disabled:cursor-not-allowed disabled:opacity-70"
             type="text"
+            value={name}
             placeholder="What's your project called?"
+            onChange={(e) => setName(e.target.value)}
+            disabled={disabledInputs}
           />
           <input
-            className="w-full rounded-full border border-white/10 py-3 pl-3 pr-5 bg-transparent outline-none focus:border-purple-500 transition-colors"
+            className="w-full rounded-full border border-white/10 py-3 pl-3 pr-5 bg-transparent outline-none focus:border-purple-500 transition-colors disabled:cursor-not-allowed disabled:opacity-70"
             type="text"
             placeholder="API base URL"
+            value={apiUrl}
+            onChange={(e) => setApiUrl(e.target.value.toLowerCase().trim())}
+            disabled={disabledInputs}
           />
-          <button className="font-semibold w-full rounded-full bg-gradient-to-l from-purple-800/90 to-purple-500 py-3 border-2 border-transparent hover:border-purple-500 transition-colors">
-            Next
+          <button
+            className="font-semibold w-full rounded-full bg-gradient-to-l from-purple-800/90 to-purple-500 py-3 border-2 border-transparent hover:border-purple-500 transition-colors disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
+            disabled={loading}
+          >
+            {loading ? <CgSpinner className="animate-spin text-2xl" /> : "Next"}
           </button>
         </form>
       </div>
