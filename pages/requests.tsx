4d74e0b10b6c9faef96b9b4d448b65a2f68c21de
akinloluwami <akinkunmioye42@gmail.com>
2023-09-03 23:15:05 +0100

    did a shit ton of things

--- pages/requests.tsx
+++ pages/requests.tsx
@@ -3,85 +3,121 @@ import RequestsTable from "@/components/RequestsTable";
 import { useEffect, useState } from "react";
 import { useProjectStore } from "@/stores/projectStore";
 import { axios } from "@/configs/axios";
-import { Flex, Select, SelectItem, TextInput } from "@tremor/react";
+import useHref from "use-href";
+
+import {
+  Flex,
+  MultiSelect,
+  MultiSelectItem,
+  SearchSelect,
+  SearchSelectItem,
+  Select,
+  SelectItem,
+  TextInput,
+} from "@tremor/react";
 
 const Requests = () => {
   const [requests, setRequests] = useState([]);
   const { project } = useProjectStore();
 
+  const [search, setSearch] = useState("");
+  const [methods, setMethods] = useState([]);
+  const [statusCodes, setStatusCodes] = useState<string[]>([]);
+  const { addQueryParam } = useHref();
+
   useEffect(() => {
     (async () => {
       try {
-        const { data } = await axios(`/logs?projectId=${project?.id}`);
+        const encodedURL = `/logs?projectId=${encodeURIComponent(
+          `${project.id}`
+        )}&status_codes=${encodeURIComponent(
+          statusCodes.join("_")
+        )}&methods=${encodeURIComponent(
+          methods.join("_")
+        )}&search=${encodeURIComponent(search)}`;
+
+        const { data } = await axios(encodedURL);
         setRequests(data);
       } catch (error) {}
     })();
-  }, [project.id]);
+  }, [project.id, statusCodes, methods]);
 
-  const httpStatusCodes = {
-    100: "Continue",
-    101: "Switching Protocols",
-    102: "Processing",
-    200: "OK",
-    201: "Created",
-    202: "Accepted",
-    203: "Non-Authoritative Information",
-    204: "No Content",
-    205: "Reset Content",
-    206: "Partial Content",
-    207: "Multi-Status",
-    208: "Already Reported",
-    226: "IM Used",
-    300: "Multiple Choices",
-    301: "Moved Permanently",
-    302: "Found",
-    303: "See Other",
-    304: "Not Modified",
-    305: "Use Proxy",
-    306: "Switch Proxy",
-    307: "Temporary Redirect",
-    308: "Permanent Redirect",
-    400: "Bad Request",
-    401: "Unauthorized",
-    402: "Payment Required",
-    403: "Forbidden",
-    404: "Not Found",
-    405: "Method Not Allowed",
-    406: "Not Acceptable",
-    407: "Proxy Authentication Required",
-    408: "Request Timeout",
-    409: "Conflict",
-    410: "Gone",
-    411: "Length Required",
-    412: "Precondition Failed",
-    413: "Payload Too Large",
-    414: "URI Too Long",
-    415: "Unsupported Media Type",
-    416: "Range Not Satisfiable",
-    417: "Expectation Failed",
-    418: "I'm a Teapot",
-    421: "Misdirected Request",
-    422: "Unprocessable Entity",
-    423: "Locked",
-    424: "Failed Dependency",
-    425: "Too Early",
-    426: "Upgrade Required",
-    428: "Precondition Required",
-    429: "Too Many Requests",
-    431: "Request Header Fields Too Large",
-    451: "Unavailable For Legal Reasons",
-    500: "Internal Server Error",
-    501: "Not Implemented",
-    502: "Bad Gateway",
-    503: "Service Unavailable",
-    504: "Gateway Timeout",
-    505: "HTTP Version Not Supported",
-    506: "Variant Also Negotiates",
-    507: "Insufficient Storage",
-    508: "Loop Detected",
-    510: "Not Extended",
-    511: "Network Authentication Required",
-  };
+  const httpStatusCodes: { code: number; name: string }[] = [
+    { code: 100, name: "Continue" },
+    { code: 101, name: "Switching Protocols" },
+    { code: 102, name: "Processing" },
+    { code: 200, name: "OK" },
+    { code: 201, name: "Created" },
+    { code: 202, name: "Accepted" },
+    { code: 203, name: "Non-Authoritative Information" },
+    { code: 204, name: "No Content" },
+    { code: 205, name: "Reset Content" },
+    { code: 206, name: "Partial Content" },
+    { code: 207, name: "Multi-Status" },
+    { code: 208, name: "Already Reported" },
+    { code: 226, name: "IM Used" },
+    { code: 300, name: "Multiple Choices" },
+    { code: 301, name: "Moved Permanently" },
+    { code: 302, name: "Found" },
+    { code: 303, name: "See Other" },
+    { code: 304, name: "Not Modified" },
+    { code: 305, name: "Use Proxy" },
+    { code: 306, name: "Switch Proxy (Unused)" },
+    { code: 307, name: "Temporary Redirect" },
+    { code: 308, name: "Permanent Redirect" },
+    { code: 400, name: "Bad Request" },
+    { code: 401, name: "Unauthorized" },
+    { code: 402, name: "Payment Required" },
+    { code: 403, name: "Forbidden" },
+    { code: 404, name: "Not Found" },
+    { code: 405, name: "Method Not Allowed" },
+    { code: 406, name: "Not Acceptable" },
+    { code: 407, name: "Proxy Authentication Required" },
+    { code: 408, name: "Request Timeout" },
+    { code: 409, name: "Conflict" },
+    { code: 410, name: "Gone" },
+    { code: 411, name: "Length Required" },
+    { code: 412, name: "Precondition Failed" },
+    { code: 413, name: "Payload Too Large" },
+    { code: 414, name: "URI Too Long" },
+    { code: 415, name: "Unsupported Media Type" },
+    { code: 416, name: "Range Not Satisfiable" },
+    { code: 417, name: "Expectation Failed" },
+    { code: 418, name: "I'm a Teapot" },
+    { code: 421, name: "Misdirected Request" },
+    { code: 422, name: "Unprocessable Entity" },
+    { code: 423, name: "Locked" },
+    { code: 424, name: "Failed Dependency" },
+    { code: 425, name: "Too Early" },
+    { code: 426, name: "Upgrade Required" },
+    { code: 428, name: "Precondition Required" },
+    { code: 429, name: "Too Many Requests" },
+    { code: 431, name: "Request Header Fields Too Large" },
+    { code: 451, name: "Unavailable For Legal Reasons" },
+    { code: 500, name: "Internal Server Error" },
+    { code: 501, name: "Not Implemented" },
+    { code: 502, name: "Bad Gateway" },
+    { code: 503, name: "Service Unavailable" },
+    { code: 504, name: "Gateway Timeout" },
+    { code: 505, name: "HTTP Version Not Supported" },
+    { code: 506, name: "Variant Also Negotiates" },
+    { code: 507, name: "Insufficient Storage" },
+    { code: 508, name: "Loop Detected" },
+    { code: 510, name: "Not Extended" },
+    { code: 511, name: "Network Authentication Required" },
+  ];
+
+  const httpMethods = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "HEAD",
+    "OPTIONS",
+    "CONNECT",
+    "TRACE",
+  ];
 
   return (
     <DashboardLayout pageTitle="Requests">
@@ -105,7 +141,44 @@ const Requests = () => {
           <SelectItem value="30d">Last 30 days</SelectItem>
           <SelectItem value="all">All time</SelectItem>
         </Select>
-        <Select defaultValue="all"></Select>
+        <MultiSelect
+          placeholder="All status codes"
+          onValueChange={(e) => {
+            let u = [];
+            u.push(e);
+            addQueryParam("status_codes", u.join("_"));
+            //@ts-ignore
+            setStatusCodes([...u]);
+          }}
+        >
+          <>
+            {httpStatusCodes.map((status) => (
+              <MultiSelectItem value={status.code.toString()} key={status.code}>
+                {status.code} {status.name}
+              </MultiSelectItem>
+            ))}
+          </>
+        </MultiSelect>
+
+        <MultiSelect
+          placeholder="All methods"
+          onValueChange={(e) => {
+            let u = [];
+            u.push(e);
+            addQueryParam("methods", u.join("_"));
+
+            //@ts-ignore
+            setMethods([...u]);
+          }}
+        >
+          <>
+            {httpMethods.map((method) => (
+              <MultiSelectItem value={method} key={method.toLowerCase()}>
+                {method}
+              </MultiSelectItem>
+            ))}
+          </>
+        </MultiSelect>
       </div>
       <RequestsTable data={requests} />
     </DashboardLayout>
