578c0a9153457fb8e7e1662609d8c5c84e9038e4
akinloluwami <akinkunmioye42@gmail.com>
2023-09-03 22:09:45 +0100

    added filters

--- pages/requests.tsx
+++ pages/requests.tsx
@@ -18,9 +18,74 @@ const Requests = () => {
     })();
   }, [project.id]);
 
+  const httpStatusCodes = {
+    100: "Continue",
+    101: "Switching Protocols",
+    102: "Processing",
+    200: "OK",
+    201: "Created",
+    202: "Accepted",
+    203: "Non-Authoritative Information",
+    204: "No Content",
+    205: "Reset Content",
+    206: "Partial Content",
+    207: "Multi-Status",
+    208: "Already Reported",
+    226: "IM Used",
+    300: "Multiple Choices",
+    301: "Moved Permanently",
+    302: "Found",
+    303: "See Other",
+    304: "Not Modified",
+    305: "Use Proxy",
+    306: "Switch Proxy",
+    307: "Temporary Redirect",
+    308: "Permanent Redirect",
+    400: "Bad Request",
+    401: "Unauthorized",
+    402: "Payment Required",
+    403: "Forbidden",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    406: "Not Acceptable",
+    407: "Proxy Authentication Required",
+    408: "Request Timeout",
+    409: "Conflict",
+    410: "Gone",
+    411: "Length Required",
+    412: "Precondition Failed",
+    413: "Payload Too Large",
+    414: "URI Too Long",
+    415: "Unsupported Media Type",
+    416: "Range Not Satisfiable",
+    417: "Expectation Failed",
+    418: "I'm a Teapot",
+    421: "Misdirected Request",
+    422: "Unprocessable Entity",
+    423: "Locked",
+    424: "Failed Dependency",
+    425: "Too Early",
+    426: "Upgrade Required",
+    428: "Precondition Required",
+    429: "Too Many Requests",
+    431: "Request Header Fields Too Large",
+    451: "Unavailable For Legal Reasons",
+    500: "Internal Server Error",
+    501: "Not Implemented",
+    502: "Bad Gateway",
+    503: "Service Unavailable",
+    504: "Gateway Timeout",
+    505: "HTTP Version Not Supported",
+    506: "Variant Also Negotiates",
+    507: "Insufficient Storage",
+    508: "Loop Detected",
+    510: "Not Extended",
+    511: "Network Authentication Required",
+  };
+
   return (
     <DashboardLayout pageTitle="Requests">
-      <div className="flex items-center w-full mb-5 gap-4">
+      <div className="flex items-center w-full mb-7 gap-4">
         <TextInput placeholder="Search..." />
         <Select defaultValue="all">
           <SelectItem className="!bg-black" value="15m">
@@ -32,28 +97,15 @@ const Requests = () => {
           <SelectItem className="!bg-black" value="ah">
             Last 1 hour
           </SelectItem>
-          <SelectItem className="!bg-black" value="10h">
-            Last 10 hours
-          </SelectItem>
-          <SelectItem className="!bg-black" value="24h">
-            Last 24 hours
-          </SelectItem>
-          <SelectItem className="!bg-black" value="3d">
-            Last 3 days
-          </SelectItem>
-          <SelectItem className="!bg-black" value="7d">
-            Last 7 days
-          </SelectItem>
-          <SelectItem className="!bg-black" value="14d">
-            Last 14 days
-          </SelectItem>
-          <SelectItem className="!bg-black" value="30d">
-            Last 30 days
-          </SelectItem>
-          <SelectItem className="!bg-black" value="all">
-            All time
-          </SelectItem>
+          <SelectItem value="10h">Last 10 hours</SelectItem>
+          <SelectItem value="24h">Last 24 hours</SelectItem>
+          <SelectItem value="3d">Last 3 days</SelectItem>
+          <SelectItem value="7d">Last 7 days</SelectItem>
+          <SelectItem value="14d">Last 14 days</SelectItem>
+          <SelectItem value="30d">Last 30 days</SelectItem>
+          <SelectItem value="all">All time</SelectItem>
         </Select>
+        <Select defaultValue="all"></Select>
       </div>
       <RequestsTable data={requests} />
     </DashboardLayout>
