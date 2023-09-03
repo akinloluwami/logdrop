5afd1367f2f89c396b43b79538ab8e1b41433253
akinloluwami <akinkunmioye42@gmail.com>
2023-09-03 21:55:13 +0100

    changed default bg color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
           background: {
             muted: "#131A2B", // custom
             subtle: "#1f2937", // gray-800
-            DEFAULT: "#111827", // gray-900
+            DEFAULT: "#000", // gray-900
             emphasis: "#d1d5db", // gray-300
           },
           border: {
