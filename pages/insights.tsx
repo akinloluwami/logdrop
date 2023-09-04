06d36cb39005d86c8959cd32f7d0799bc6c6e74b
akinloluwami <akinkunmioye42@gmail.com>
2023-09-04 21:53:09 +0100

    fetch data

--- pages/insights.tsx
+++ pages/insights.tsx
@@ -1,6 +1,26 @@
+import { axios } from "@/configs/axios";
 import DashboardLayout from "@/layouts/DashboardLayout";
+import { useProjectStore } from "@/stores/projectStore";
+import dayjs from "dayjs";
+import { useEffect } from "react";
 
 const Insights = () => {
+  const { project } = useProjectStore();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios(
+          `/stats/endpoints?projectId=${project?.id}&startDate=${dayjs().subtract(
+            6,
+            "days"
+          )}&endDate=${dayjs()}`
+        );
+        // setChartData(data);
+        console.log(data);
+      } catch (error) {}
+    })();
+  }, [project.id]);
   return <DashboardLayout pageTitle="Insights">Insights</DashboardLayout>;
 };
 
