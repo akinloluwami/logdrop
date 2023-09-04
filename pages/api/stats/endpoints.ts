9e9f1c0f12ee35a6079c388e0a3951afa70f257d
akinloluwami <akinkunmioye42@gmail.com>
2023-09-04 21:53:17 +0100

    initial commit

---
+++ pages/api/stats/endpoints.ts
@@ -0,0 +1,43 @@
+import { NextApiResponse } from "next";
+import { requestMethod } from "@/middlewares/requestMethod";
+import { CustomRequest } from "@/interfaces";
+import authenticateToken from "@/middlewares/auth";
+import { prisma } from "@/prisma";
+import dayjs from "dayjs";
+
+const handler = async (req: CustomRequest, res: NextApiResponse) => {
+  try {
+    const { startDate, endDate, projectId } = req.query;
+
+    const start = dayjs(startDate as string);
+    const end = dayjs(endDate as string);
+
+    if (projectId === "null") {
+      res.status(400).json({ message: "Project ID is required" });
+      return;
+    }
+
+    const endpoints = await prisma.log.groupBy({
+      by: ["endpoint"],
+      where: {
+        projectId: Number(projectId),
+        createdAt: {
+          gte: start.toISOString(),
+          lte: end.toISOString(),
+        },
+      },
+      _count: true,
+    });
+    res.status(200).json(endpoints);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+export default requestMethod(["GET"])((
+  req: CustomRequest,
+  res: NextApiResponse
+) => {
+  authenticateToken(req, res, () => handler(req, res));
+});
