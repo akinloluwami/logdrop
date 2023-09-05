00b641c72c582d5036e1f768c5d11beae108603e
akinloluwami <akinkunmioye42@gmail.com>
2023-09-05 02:00:46 +0100

    fetch data

--- pages/logs/[uuid].tsx
+++ pages/logs/[uuid].tsx
@@ -1,7 +1,20 @@
+import { axios } from "@/configs/axios";
 import DashboardLayout from "@/layouts/DashboardLayout";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 const Log = () => {
+  const router = useRouter();
+  const [log, setLog] = useState<any>({});
+
+  useEffect(() => {
+    console.log(router?.query?.uuid);
+    (async () => {
+      const { data } = await axios(`/logs/${router?.query?.uuid}`);
+      console.log(data);
+    })();
+  }, [router?.query]);
+
   return <DashboardLayout pageTitle="Log">Log</DashboardLayout>;
 };
 
