1aa2447f25e16c6a8903067f7a25d608a78b24be
akinloluwami <akinkunmioye42@gmail.com>
2023-09-05 01:06:34 +0100

    .

---
+++ pages/api/stats/os.ts
@@ -0,0 +1,54 @@
+import { NextApiResponse } from "next";
+import { requestMethod } from "@/middlewares/requestMethod";
+import { CustomRequest } from "@/interfaces";
+import authenticateToken from "@/middlewares/auth";
+import { prisma } from "@/prisma";
+import dayjs from "dayjs";
+
+const handler = async (req: CustomRequest, res: NextApiResponse) => {
+  try {
+    const { startDate, endDate, projectId, take } = req.query;
+
+    const start = dayjs(startDate as string);
+    const end = dayjs(endDate as string);
+
+    if (projectId === "null") {
+      res.status(400).json({ message: "Project ID is required" });
+      return;
+    }
+
+    const os = await prisma.log.groupBy({
+      by: ["os"],
+      where: {
+        projectId: Number(projectId),
+        createdAt: {
+          gte: start.toISOString(),
+          lte: end.toISOString(),
+        },
+      },
+      _count: true,
+      orderBy: {
+        _count: {
+          endpoint: "desc",
+        },
+      },
+      take: Number(take),
+    });
+
+    const mod = os.map(({ os: name, _count: value }) => ({
+      name,
+      value,
+    }));
+    res.status(200).json(mod);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+export default requestMethod(["GET"])((
+  req: CustomRequest,
+  res: NextApiResponse
+) => {
+  authenticateToken(req, res, () => handler(req, res));
+});
