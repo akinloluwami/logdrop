8a9443f18f89fca9683143fec98d5e01c793ad59
akinloluwami <akinkunmioye42@gmail.com>
2023-09-05 11:54:36 +0100

    return "<1ms" if ms is 0

--- utils/formatTimeTaken.ts
+++ utils/formatTimeTaken.ts
@@ -1,8 +1,9 @@
 export const formatTimeTaken = (ms: number) => {
+  if (ms === 0) return "<1ms";
+
   if (ms > 1000) {
     const secondsValue = ms / 1000;
     return secondsValue.toFixed(2) + "s";
-  } else {
-    return ms + "ms";
   }
+  return ms + "ms";
 };
