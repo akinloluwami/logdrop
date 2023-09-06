eb009815cd686db0dd6c946c0f5a9750d7c4a183
akinloluwami <akinkunmioye42@gmail.com>
2023-09-06 01:57:50 +0100

    setup analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import PlausibleProvider from "next-plausible";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="reqlog.co">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
