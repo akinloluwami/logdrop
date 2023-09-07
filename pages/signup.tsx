78c8a2461bb7ae67aa36e0ff82adbdf495508597
akinloluwami <akinkunmioye42@gmail.com>
2023-09-07 08:56:35 +0100

    added loading state

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,15 +1,19 @@
 import AuthLayout from "../layouts/AuthLayout";
 import GitHubButton from "../components/GitHubButton";
 import Head from "next/head";
+import { useState } from "react";
 
 const Signup = () => {
+  const [loading, setLoading] = useState(false);
   return (
     <AuthLayout>
       <Head>
         <title>Signup • ReqLog</title>
       </Head>
       <GitHubButton
+        loading={loading}
         action={() => {
+          setLoading(true);
           window.open(`api/auth/github`, "_self");
         }}
       />
