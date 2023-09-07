7ff20b4ec96a59f88e79745ee32a6da5a57a4799
akinloluwami <akinkunmioye42@gmail.com>
2023-09-07 09:02:12 +0100

    added toast component globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import PlausibleProvider from "next-plausible";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PlausibleProvider domain="reqlog.co">
+      <Toaster />
       <Component {...pageProps} />
     </PlausibleProvider>
   );
