e2fe58531688adfac98e48bcde784d336d46a79f
akinloluwami <akinkunmioye42@gmail.com>
2023-09-07 08:26:57 +0100

    get API key

--- pages/api/api-key.ts
+++ pages/api/api-key.ts
@@ -6,39 +6,59 @@ import { prisma } from "@/prisma";
 
 const handler = async (req: CustomRequest, res: NextApiResponse) => {
   try {
-    const project = await prisma.project.findFirst({
-      where: {
-        userId: req.user?.id,
-      },
-    });
+    if (req.method === "POST") {
+      const project = await prisma.project.findFirst({
+        where: {
+          userId: req.user?.id,
+        },
+      });
 
-    const generateApiKey = () => {
-      const characters =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      const generateApiKey = () => {
+        const characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-      const keyLength = 32;
+        const keyLength = 32;
 
-      let apiKey = "";
+        let apiKey = "";
 
-      for (let i = 0; i < keyLength; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        apiKey += characters.charAt(randomIndex);
-      }
+        for (let i = 0; i < keyLength; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          apiKey += characters.charAt(randomIndex);
+        }
 
-      return apiKey;
-    };
+        return apiKey;
+      };
 
-    const apiKey = await prisma.aPiKey.create({
-      data: {
-        projectId: project?.id!,
-        key: generateApiKey(),
-      },
-    });
-    res.json({ key: apiKey.key });
-  } catch (error) {}
+      const apiKey = await prisma.aPiKey.create({
+        data: {
+          projectId: project?.id!,
+          key: generateApiKey(),
+        },
+      });
+      res.json({ key: apiKey.key });
+      return;
+    }
+    if (req.method === "GET") {
+      const projectId = Number(req.query.projectId);
+
+      const apiKey = await prisma.aPiKey.findFirst({
+        where: {
+          projectId,
+        },
+        select: {
+          key: true,
+        },
+      });
+      res.status(200).json(apiKey?.key);
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
-export default requestMethod(["POST"])((
+export default requestMethod(["GET", "POST"])((
   req: CustomRequest,
   res: NextApiResponse
 ) => {
