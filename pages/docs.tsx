a66a0321d5b8cc5afb42604251bf32f1f2d8066f
akinloluwami <akinkunmioye42@gmail.com>
2023-09-07 08:56:09 +0100

    initial commit

---
+++ pages/docs.tsx
@@ -0,0 +1,12 @@
+import Cooking from "@/components/Cooking";
+import React from "react";
+
+const Docs = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Cooking />
+    </div>
+  );
+};
+
+export default Docs;
