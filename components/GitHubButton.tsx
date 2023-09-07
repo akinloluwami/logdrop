fa6197c7dd83fbd008b034c3392d762352b5eb48
akinloluwami <akinkunmioye42@gmail.com>
2023-09-07 08:55:13 +0100

    added loading state

--- components/GitHubButton.tsx
+++ components/GitHubButton.tsx
@@ -1,13 +1,25 @@
+import { CgSpinner } from "react-icons/cg";
 import { SiGithub } from "react-icons/si";
 
-const GitHubButton = ({ action }: { action: () => void }) => {
+const GitHubButton = ({
+  action,
+  loading,
+}: {
+  action: () => void;
+  loading: boolean;
+}) => {
   return (
     <button
       onClick={action}
-      className="flex items-center gap-3 bg-white/10 px-16 py-4 rounded-full font-semibold bg-gradient-to-r from-transparent to-purple-500/50  hover:to-purple-500 transition-all"
+      className="flex items-center justify-center gap-3 bg-white/10 px-16 py-4 rounded-full font-semibold bg-gradient-to-r w-[350px] from-transparent to-purple-500/50  hover:to-purple-500 transition-all disabled:cursor-not-allowed disabled:opacity-60"
+      disabled={loading}
     >
       <SiGithub />
-      Continue with GitHub
+      {loading ? (
+        <CgSpinner className="animate-spin" size={20} />
+      ) : (
+        "Continue with GitHub"
+      )}
     </button>
   );
 };
