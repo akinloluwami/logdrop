97b0e522ff66c475b7a95352fab92e4a169af84c
akinloluwami <akinkunmioye42@gmail.com>
2023-09-08 03:37:40 +0100

    .

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -46,7 +46,7 @@ const DashboardLayout: FC<Props> = ({ children, pageTitle }) => {
             <div className="hidden lg:block border-r border-gray-800 w-[20%] fixed">
               <Sidebar />
             </div>
-            <div className="w-[80%] ml-[20%]">
+            <div className="lg:w-[80%] lg:ml-[20%]">
               <div className="flex justify-between border-b border-gray-800 px-10 py-5 w-full sticky top-0 bg-black/20 backdrop-blur-md z-50">
                 <h2 className="text-2xl font-medium">{pageTitle}</h2>
                 <button>Docs</button>
