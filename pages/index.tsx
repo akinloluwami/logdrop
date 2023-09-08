1fb9dd33956796c50f2fdd3ab19b76defa76e3d2
akinloluwami <akinkunmioye42@gmail.com>
2023-09-08 16:09:32 +0100

    added page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Hero />
+      <Head>
+        <title>ReqLog • Analytics, logging and alerts for Express APIs</title>
+      </Head>
+      <>
+        <Navbar />
+        <Hero />
+      </>
     </>
   );
 }
