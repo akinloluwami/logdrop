ecce03f44dfbe9a4d647e10650b75193ac6e550d
akinloluwami <akinkunmioye42@gmail.com>
2023-09-08 01:29:35 +0100

    apiUrl

--- stores/projectStore.tsx
+++ stores/projectStore.tsx
@@ -3,17 +3,19 @@ import { create } from "zustand";
 interface ProjectStore {
   project: {
     name: string;
+    apiUrl: string;
     id: number | null;
   };
-  setProject: (name: string, id: number) => void;
+  setProject: (name: string, id: number, apiUrl: string) => void;
 }
 
 export const useProjectStore = create<ProjectStore>((set) => ({
   project: {
-    name: "",
     id: null,
+    name: "",
+    apiUrl: "",
   },
-  setProject: (name, id) => {
-    set({ project: { name, id } });
+  setProject: (name, id, apiUrl) => {
+    set({ project: { name, id, apiUrl } });
   },
 }));
