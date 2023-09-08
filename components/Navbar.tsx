565c21037c0500505978ea80cd2723ef21fd03e6
akinloluwami <akinkunmioye42@gmail.com>
2023-09-08 02:48:07 +0100

    responsive menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Logo from "./Logo";
 import { usePathname } from "next/navigation";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
   const links = [
@@ -8,12 +11,13 @@ const Navbar = () => {
     { title: "Docs", href: "/docs" },
   ];
 
+  const [showMenu, setShowMenu] = useState(false);
   const pathname = usePathname();
 
   return (
-    <div className="flex justify-between px-14 py-4">
+    <div className="flex justify-between lg:px-14 px-4 py-4 items-center relative">
       <Logo />
-      <div className="flex gap-12 items-center font-semibold">
+      <div className="hidden lg:flex gap-12 items-center font-semibold">
         {links.map((link, i) => (
           <Link
             key={i}
@@ -26,7 +30,7 @@ const Navbar = () => {
           </Link>
         ))}
       </div>
-      <div className="flex gap-12 items-center font-semibold">
+      <div className="hidden lg:flex gap-12 items-center font-semibold">
         <Link
           href={"/login"}
           className="hover:text-purple-500 transition-colors"
@@ -40,6 +44,43 @@ const Navbar = () => {
         >
           Get started
         </Link>
+      </div>
+      <button className="lg:hidden" onClick={() => setShowMenu(true)}>
+        <HiMenuAlt4 className="text-2xl" color="#fff" size={30} />
+      </button>
+      <div
+        className={`${
+          showMenu
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        } lg:hidden bg-black z-10 top-0 right-0 h-screen w-full absolute transition-opacity duration-500`}
+      >
+        <div className="flex flex-col gap-6 p-8 text-white text-center mt-20">
+          <button
+            className="absolute top-4 right-4 text-2xl"
+            onClick={() => setShowMenu(false)}
+          >
+            <IoClose />
+          </button>
+          {links.map((link, i) => (
+            <Link key={i} href={link.href} className="text-xl">
+              {link.title}
+            </Link>
+          ))}
+          <Link
+            href={"/login"}
+            className="bg-white px-4 py-4 font-semibold rounded-full text-black"
+          >
+            Login
+          </Link>
+
+          <Link
+            href={"/signup"}
+            className="bg-gradient-to-r from-purple-500/20 to-purple-800 px-4 py-4 font-semibold rounded-full text-white"
+          >
+            Get started
+          </Link>
+        </div>
       </div>
     </div>
   );
