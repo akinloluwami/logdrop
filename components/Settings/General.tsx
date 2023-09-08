8835e1b64fb16cc63116ab421cafa93000e8fce7
akinloluwami <akinkunmioye42@gmail.com>
2023-09-08 01:27:49 +0100

    added project api url and name

--- components/Settings/General.tsx
+++ components/Settings/General.tsx
@@ -1,23 +1,25 @@
+import { useProjectStore } from "@/stores/projectStore";
 import { Button, Flex, Text, TextInput } from "@tremor/react";
 
 const General = () => {
+  const { project } = useProjectStore();
   return (
     <div className="w-[450px] flex flex-col gap-4">
       <div className="">
         <p className="text-gray-300">Project name</p>
         <Flex className="gap-2">
           <TextInput
-            placeholder="Project name"
+            placeholder={project.name}
             className="!bg-transparent my-2"
           />
           <Button color="purple">Save</Button>
         </Flex>
       </div>
       <div className="">
-        <p className="text-gray-300">API endpoint</p>
+        <p className="text-gray-300">API URL</p>
         <Flex className="gap-2">
           <TextInput
-            placeholder="API endpoint"
+            placeholder={project.apiUrl}
             className="!bg-transparent my-2"
           />
           <Button color="purple">Save</Button>
