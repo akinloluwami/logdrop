5c5d9268eff9cf08232d65add0327c79f4b76563
akinloluwami <akinkunmioye42@gmail.com>
2023-09-08 03:35:24 +0100

    ughhh

--- pages/onboarding/ready.tsx
+++ pages/onboarding/ready.tsx
@@ -5,15 +5,17 @@ import React from "react";
 const Ready = () => {
   return (
     <OnboardingLayout>
-      <div className="flex flex-col itesm-center px-5 w-full">
+      <div className="w-full px-5">
         <h3 className="text-center text-3xl font-semibold mt-4">
           You're all set.
         </h3>
-        <Link href={"/overview"}>
-          <button className="bg-gradient-to-l from-purple-900 to-purple-500 w-full py-3 text-white px-4 rounded-full mt-10 font-semibold">
-            Continue to Dashboard
-          </button>
-        </Link>
+        <center>
+          <Link href={"/overview"} className="w-full lg:w-[450px]">
+            <button className="w-full lg:w-[450px] bg-gradient-to-l from-purple-900 to-purple-500 py-3 text-white px-4 rounded-full mt-10 font-semibold">
+              Continue to Dashboard
+            </button>
+          </Link>
+        </center>
       </div>
     </OnboardingLayout>
   );
