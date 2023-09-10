2352a4ed55f074a64236be7cff30ebb622f106b0
akinloluwami <akinkunmioye42@gmail.com>
2023-09-10 13:46:31 +0100

    initial commit

---
+++ pages/api/project/[id]/events/index.ts
@@ -0,0 +1,35 @@
+import { NextApiResponse } from "next";
+import { requestMethod } from "@/middlewares/requestMethod";
+import { CustomRequest } from "@/interfaces";
+import authenticateToken from "@/middlewares/auth";
+import { prisma } from "@/prisma";
+
+const handler = async (req: CustomRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === "POST") {
+      const projectId = Number(req.query.id);
+
+      const { name, conditions, action } = req.body;
+
+      const newEvent = await prisma.event.create({
+        data: {
+          name,
+          conditions,
+          action,
+          projectId,
+        },
+      });
+
+      res.status(201).json(newEvent);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export default requestMethod(["POST"])((
+  req: CustomRequest,
+  res: NextApiResponse
+) => {
+  authenticateToken(req, res, () => handler(req, res));
+});
