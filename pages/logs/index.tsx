1bf538de66de2a1f2b61b56f48ec63df7d44cc37
akinloluwami <akinkunmioye42@gmail.com>
2023-09-10 03:03:02 +0100

    added pagination

--- pages/logs/index.tsx
+++ pages/logs/index.tsx
@@ -15,19 +15,9 @@ import {
   TextInput,
 } from "@tremor/react";
 import { useRouter } from "next/router";
-import { httpStatusCodes } from "@/lib/statusCodes";
 
 const Requests = () => {
-  const [requests, setRequests] = useState<
-    {
-      uuid: string;
-      endpoint: string;
-      method: string;
-      statusCode: number;
-      elapsedDuration: number;
-      createdAt: string;
-    }[]
-  >([]);
+  const [requests, setRequests] = useState([]);
   const { project } = useProjectStore();
 
   const [methods, setMethods] = useState<string[]>([]);
@@ -40,6 +30,71 @@ const Requests = () => {
   }, 500);
 
   const { addQueryParam, deleteQueryParam } = useHref();
+
+  const httpStatusCodes: { code: number; name: string }[] = [
+    { code: 100, name: "Continue" },
+    { code: 101, name: "Switching Protocols" },
+    { code: 102, name: "Processing" },
+    { code: 200, name: "OK" },
+    { code: 201, name: "Created" },
+    { code: 202, name: "Accepted" },
+    { code: 203, name: "Non-Authoritative Information" },
+    { code: 204, name: "No Content" },
+    { code: 205, name: "Reset Content" },
+    { code: 206, name: "Partial Content" },
+    { code: 207, name: "Multi-Status" },
+    { code: 208, name: "Already Reported" },
+    { code: 226, name: "IM Used" },
+    { code: 300, name: "Multiple Choices" },
+    { code: 301, name: "Moved Permanently" },
+    { code: 302, name: "Found" },
+    { code: 303, name: "See Other" },
+    { code: 304, name: "Not Modified" },
+    { code: 305, name: "Use Proxy" },
+    { code: 306, name: "Switch Proxy (Unused)" },
+    { code: 307, name: "Temporary Redirect" },
+    { code: 308, name: "Permanent Redirect" },
+    { code: 400, name: "Bad Request" },
+    { code: 401, name: "Unauthorized" },
+    { code: 402, name: "Payment Required" },
+    { code: 403, name: "Forbidden" },
+    { code: 404, name: "Not Found" },
+    { code: 405, name: "Method Not Allowed" },
+    { code: 406, name: "Not Acceptable" },
+    { code: 407, name: "Proxy Authentication Required" },
+    { code: 408, name: "Request Timeout" },
+    { code: 409, name: "Conflict" },
+    { code: 410, name: "Gone" },
+    { code: 411, name: "Length Required" },
+    { code: 412, name: "Precondition Failed" },
+    { code: 413, name: "Payload Too Large" },
+    { code: 414, name: "URI Too Long" },
+    { code: 415, name: "Unsupported Media Type" },
+    { code: 416, name: "Range Not Satisfiable" },
+    { code: 417, name: "Expectation Failed" },
+    { code: 418, name: "I'm a Teapot" },
+    { code: 421, name: "Misdirected Request" },
+    { code: 422, name: "Unprocessable Entity" },
+    { code: 423, name: "Locked" },
+    { code: 424, name: "Failed Dependency" },
+    { code: 425, name: "Too Early" },
+    { code: 426, name: "Upgrade Required" },
+    { code: 428, name: "Precondition Required" },
+    { code: 429, name: "Too Many Requests" },
+    { code: 431, name: "Request Header Fields Too Large" },
+    { code: 451, name: "Unavailable For Legal Reasons" },
+    { code: 500, name: "Internal Server Error" },
+    { code: 501, name: "Not Implemented" },
+    { code: 502, name: "Bad Gateway" },
+    { code: 503, name: "Service Unavailable" },
+    { code: 504, name: "Gateway Timeout" },
+    { code: 505, name: "HTTP Version Not Supported" },
+    { code: 506, name: "Variant Also Negotiates" },
+    { code: 507, name: "Insufficient Storage" },
+    { code: 508, name: "Loop Detected" },
+    { code: 510, name: "Not Extended" },
+    { code: 511, name: "Network Authentication Required" },
+  ];
 
   const httpMethods = [
     "GET",
@@ -103,7 +158,7 @@ const Requests = () => {
           selectedDateRange
         )}&pageSize=${20}&page=${page}`;
         const { data } = await axios(encodedURL);
-        setRequests([...requests, ...data]);
+        setRequests(data);
         setRequestsLength(data.length);
       } catch (error) {}
     })();
@@ -170,17 +225,20 @@ const Requests = () => {
       </div>
       <RequestsTable data={requests} />
       <div className="mt-5 flex gap-5">
-        {requestsLength === null || requestsLength < 20 ? (
-          <p>You have reached the end of the list :)</p>
-        ) : (
-          <Button
-            onClick={() => setPage(page + 1)}
-            disabled={requestsLength === null || requestsLength < 20}
-            color="purple"
-          >
-            Load more
-          </Button>
-        )}
+        <Button
+          color="purple"
+          onClick={() => setPage(page - 1)}
+          disabled={page === 0}
+        >
+          Prev
+        </Button>
+        <Button
+          color="purple"
+          onClick={() => setPage(page + 1)}
+          disabled={requestsLength === null || requestsLength < 20}
+        >
+          Next
+        </Button>
       </div>
     </DashboardLayout>
   );
