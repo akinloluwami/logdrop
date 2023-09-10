2818f1cd3c95ede2ceff5690f61f0d03fac8a49d
akinloluwami <akinkunmioye42@gmail.com>
2023-09-10 22:49:20 +0100

    evemt table

---
+++ components/EventsTable.tsx
@@ -0,0 +1,81 @@
+import { FC } from "react";
+import {
+  Card,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  TableBody,
+  TableCell,
+  Text,
+  Title,
+  Badge,
+} from "@tremor/react";
+import moment from "moment";
+import Link from "next/link";
+import { formatTimeTaken } from "@/utils/formatTimeTaken";
+
+interface Props {
+  events: {
+    id: number;
+    name: string;
+    action: string;
+    conditions: [];
+    createdAt: string;
+    timesTriggered: number;
+    lastTriggered: string;
+  }[];
+}
+
+const head = [
+  "Name",
+  "Action",
+  "Conditions",
+  "Created At",
+  "Times Triggered",
+  "Last Triggered",
+];
+
+const EventsTable: FC<Props> = ({ events }) => {
+  return (
+    <Card className="!bg-transparent">
+      <Table className="">
+        <TableHead>
+          <TableRow>
+            {head.map((item, i) => (
+              <TableHeaderCell className="font-semibold" key={i}>
+                {item}
+              </TableHeaderCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {events.map((item, i) => (
+            <TableRow key={i}>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>
+                <Text className="font-semibold">{item.action}</Text>
+              </TableCell>
+              <TableCell>{item.conditions.length}</TableCell>
+              <TableCell>
+                <Text>{moment(item.createdAt).fromNow()}</Text>
+              </TableCell>
+              <TableCell>
+                <Text>{item.timesTriggered}</Text>
+              </TableCell>
+              <TableCell>
+                <Text>
+                  {(item.lastTriggered &&
+                    moment(item.lastTriggered).fromNow()) ||
+                    "Never"}
+                </Text>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
+  );
+};
+
+export default EventsTable;
