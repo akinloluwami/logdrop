8755177eb680d346cc576786a90cc8eeef76ac6e
akinloluwami <akinkunmioye42@gmail.com>
2023-09-10 02:32:02 +0100

    replaced with logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/">
-      <h2>LogDrop</h2>
+      <Image src={"/logo.svg"} width={120} height={50} alt="LogDrop logo" />
     </Link>
   );
 };
