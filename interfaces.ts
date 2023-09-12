0dc7902be8551940e84e9ba710f3f7957eaf00d8
akinloluwami <akinkunmioye42@gmail.com>
2023-09-12 12:24:34 +0100

    created IpInfo interface

--- interfaces.ts
+++ interfaces.ts
@@ -13,3 +13,42 @@ export interface EventProps {
   timesTriggered: number;
   lastTriggered: string;
 }
+
+export interface IpInfo {
+  ip: string;
+  success: boolean;
+  type: string;
+  continent: string;
+  continent_code: string;
+  country: string;
+  country_code: string;
+  region: string;
+  region_code: string;
+  city: string;
+  latitude: number;
+  longitude: number;
+  is_eu: boolean;
+  postal: string;
+  calling_code: string;
+  capital: string;
+  borders: string;
+  flag: {
+    img: string;
+    emoji: string;
+    emoji_unicode: string;
+  };
+  connection: {
+    asn: number;
+    org: string;
+    isp: string;
+    domain: string;
+  };
+  timezone: {
+    id: string;
+    abbr: string;
+    is_dst: boolean;
+    offset: number;
+    utc: string;
+    current_time: string;
+  };
+}
