a765cdc438ddcc7dd7f0c938b75c13f7a260595b
akinloluwami <akinkunmioye42@gmail.com>
2023-09-12 12:44:22 +0100

    event

---
+++ pages/api/project/[id]/events/[event_id].ts
@@ -0,0 +1,27 @@
+import { NextApiResponse } from "next";
+import { requestMethod } from "@/middlewares/requestMethod";
+import { CustomRequest } from "@/interfaces";
+import authenticateToken from "@/middlewares/auth";
+import { prisma } from "@/prisma";
+
+const handler = async (req: CustomRequest, res: NextApiResponse) => {
+  try {
+    const eventId = req.query.event_id;
+    if (req.method === "DELETE") {
+      await prisma.event.delete({
+        where: { id: Number(eventId) },
+      });
+      res.status(200).json({ message: "Event deleted" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export default requestMethod(["DELETE"])((
+  req: CustomRequest,
+  res: NextApiResponse
+) => {
+  authenticateToken(req, res, () => handler(req, res));
+});
