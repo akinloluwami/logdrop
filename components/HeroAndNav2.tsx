c1765806d85616fafec2a1a0d31a9ccb36df9b17
akinloluwami <akinkunmioye42@gmail.com>
2023-09-14 00:10:28 +0100

    increased bottom padding

--- components/HeroAndNav2.tsx
+++ components/HeroAndNav2.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar2";
 
 const HeroAndNav2 = () => {
   return (
-    <div className="bg-gradient-to-r from-purple-400 to-purple-700 pt-6 pb-12">
+    <div className="bg-gradient-to-r from-purple-400 to-purple-700 pt-6 pb-20">
       <Navbar />
       <Hero />
     </div>
