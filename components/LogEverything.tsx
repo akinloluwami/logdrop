7816982e4fbe1110a6099acf2891112d4ff859cb
akinloluwami <akinkunmioye42@gmail.com>
2023-09-13 18:41:38 +0100

    mobile...

--- components/LogEverything.tsx
+++ components/LogEverything.tsx
@@ -3,12 +3,12 @@ const LogEverything = () => {
   return (
     <div className="lg:px-10 px-5 my-10">
       <div className="">
-        <h1 className="lg:text-9xl text-5xl font-semibold text-center my-10">
+        <h1 className="lg:text-9xl text-5xl font-semibold text-center lg:my-10 my-5">
           Log everything.
         </h1>
       </div>
       <Tilt glareColor="#ffffff" tiltMaxAngleX={-2} tiltMaxAngleY={2}>
-        <div className="w-full bg-gradient-to-r from-purple-300 to-purple-800 p-5 rounded-lg">
+        <div className="w-full bg-gradient-to-r from-purple-300 to-purple-800 lg:p-5 p-1.5 rounded-lg">
           <img
             src={"/screenshot.png"}
             alt="Log errthing"
