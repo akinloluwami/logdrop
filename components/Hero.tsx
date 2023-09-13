13d34c9ad486e6b490a2b1cd619ef274ebc7c255
akinloluwami <akinkunmioye42@gmail.com>
2023-09-13 13:25:49 +0100

    removed gradient text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className="flex items-center flex-col bg-gradient-to-b from-transparent via-purple-500/20 to-transparent h-[80vh]">
       <Link
         href={"/github"}
-        className="group mx-auto flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 px-7 py-2 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.1)] backdrop-blur transition-all hover:border-gray-300 hover:bg-purple-800/30 mt-10"
+        className="group mx-auto flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 px-7 py-2 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.1)] backdrop-blur transition-all hover:border-gray-300 hover:bg-purple-800/20 mt-10"
       >
         <SiGithub />
         <p className="text-sm font-semibold">
@@ -16,7 +16,7 @@ const Hero = () => {
         </p>
         <ExpandingArrow className="-ml-1 h-3.5 w-3.5" />
       </Link>
-      <h1 className="mt-10 mx-6 lg:w-[60%] font-semibold text-4xl lg:text-7xl text-center mb-4 bg-clip-text text-transparent bg-gradient-to-b from-black/80 to-black dark:from-white dark:to-[#AAAAAA] h-fit">
+      <h1 className="mt-10 mx-6 lg:w-[60%] font-bold text-4xl lg:text-7xl text-center mb-4 h-fit">
         Powerful analytics for NodeJS APIs
       </h1>
       <p className="lg:text-xl text-lg lg:w-[50%] px-5 text-center text-gray-300">
