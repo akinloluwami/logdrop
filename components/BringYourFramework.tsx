a82005b1d1abd1484639f71893765326fcba85f6
akinloluwami <akinkunmioye42@gmail.com>
2023-09-13 03:13:36 +0100

    initial commit

---
+++ components/BringYourFramework.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { PiFireSimpleFill } from "react-icons/pi";
+import {
+  SiAdonisjs,
+  SiExpress,
+  SiFastify,
+  SiKoa,
+  SiMeteor,
+} from "react-icons/si";
+
+const BringYourFramework = () => {
+  const frameworks = [
+    {
+      name: "Express",
+      icon: <SiExpress />,
+    },
+    {
+      name: "Fastify",
+      icon: <SiFastify />,
+    },
+    {
+      name: "Adonis",
+      icon: <SiAdonisjs />,
+    },
+    {
+      name: "Meteor",
+      icon: <SiMeteor />,
+    },
+    {
+      name: "Hono",
+      icon: <PiFireSimpleFill />,
+    },
+    {
+      name: "Koa",
+      icon: <SiKoa />,
+    },
+  ];
+  const [selected, setSelected] = useState(frameworks[0]);
+  return (
+    <div className="my-10">
+      <h1 className="text-center font-semibold text-6xl">
+        Bring your{" "}
+        <span className="bg-gradient-to-r from from-purple-300 to-purple-900 bg-clip-text text-transparent">
+          favourite
+        </span>{" "}
+        framework{" "}
+      </h1>
+      <div className="mt-10">
+        <div className="flex items-center gap-16 justify-center">
+          {frameworks.map((framework, i) => (
+            <div
+              className="flex items-center flex-col"
+              key={i}
+              onClick={() => setSelected(framework)}
+            >
+              <div
+                className={`${
+                  selected.name === framework.name
+                    ? "bg-gradient-to-bl from-slate-300 via-slate-600 to-purple-700"
+                    : "bg-slate-300/20 hover:bg-purple-800/40"
+                } p-[1.5px] rounded-lg cursor-pointer`}
+              >
+                <div className="text-3xl bg-black rounded-md p-5">
+                  {framework.icon}
+                </div>
+              </div>
+              <p>{framework.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BringYourFramework;
