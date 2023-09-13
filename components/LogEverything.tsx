f2ef584a61da73d364653f9b07aca512ca425c5f
akinloluwami <akinkunmioye42@gmail.com>
2023-09-13 22:56:34 +0100

    ...

--- components/LogEverything.tsx
+++ components/LogEverything.tsx
@@ -1,21 +1,37 @@
 import Tilt from "react-parallax-tilt";
 const LogEverything = () => {
+  const features = [
+    { title: "Log everything", image: "/screenshot.png" },
+    {
+      title: "Insights on each request",
+      image: "/screenshot2.png",
+    },
+    {
+      title: "Get notified about events",
+      image: "/screenshot3.png",
+    },
+  ];
+
   return (
-    <div className="lg:px-10 px-5 my-10">
-      <div className="">
-        <h1 className="lg:text-7xl text-5xl font-semibold text-center lg:my-10 my-5">
-          Log everything.
-        </h1>
-      </div>
-      <Tilt glareColor="#ffffff" tiltMaxAngleX={-2} tiltMaxAngleY={2}>
-        <div className="w-full bg-gradient-to-r from-purple-300 to-purple-800 lg:p-5 p-1.5 rounded-lg">
-          <img
-            src={"/screenshot.png"}
-            alt="Log errthing"
-            className="rounded-lg w-full"
-          />
+    <div className="flex flex-col gap-8">
+      {features.map((feature, i) => (
+        <div className="lg:px-10 px-5 my-10" key={i}>
+          <div className="">
+            <h1 className="lg:text-7xl text-5xl font-semibold text-center lg:my-10 my-5">
+              {feature.title}
+            </h1>
+          </div>
+          <Tilt glareColor="#ffffff" tiltMaxAngleX={-2} tiltMaxAngleY={2}>
+            <div className="w-full bg-gradient-to-r from-purple-300 to-purple-800 lg:p-5 p-1.5 rounded-lg">
+              <img
+                src={feature.image}
+                alt="feature"
+                className="rounded-lg w-full"
+              />
+            </div>
+          </Tilt>
         </div>
-      </Tilt>
+      ))}
     </div>
   );
 };
