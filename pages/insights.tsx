f8da96bbb4d4df352198014a6cfc3029b1e03313
akinloluwami <akinkunmioye42@gmail.com>
2023-09-14 13:06:06 +0100

    added new datasets

--- pages/insights.tsx
+++ pages/insights.tsx
@@ -25,48 +25,54 @@ const Insights = () => {
   >([]);
   const [operatingSystems, setOperatingSystems] = useState([]);
   const [browsers, setBrowsers] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [cities, setCities] = useState([]);
 
   const take = 6;
 
+  const url = `/project/${project?.id}/insights?startDate=${dayjs().subtract(
+    6,
+    "days"
+  )}&endDate=${dayjs()}&take=${take}&group=`;
+
   useEffect(() => {
+    if (project?.id) {
+      (async () => {
+        try {
+          const { data } = await axios(`${url}endpoint`);
+          setTopEndpoints(data);
+        } catch (error) {}
+      })();
+    }
     (async () => {
       try {
-        const { data } = await axios(
-          `/stats/endpoints?projectId=${project?.id}&startDate=${dayjs().subtract(
-            6,
-            "days"
-          )}&endDate=${dayjs()}&take=${take}`
-        );
-        setTopEndpoints(data);
-      } catch (error) {}
-    })();
-    (async () => {
-      try {
-        const { data } = await axios(
-          `/stats/os?projectId=${project?.id}&startDate=${dayjs().subtract(
-            6,
-            "days"
-          )}&endDate=${dayjs()}&take=${take}`
-        );
+        const { data } = await axios(`${url}os`);
         setOperatingSystems(data);
       } catch (error) {}
     })();
     (async () => {
       try {
-        const { data } = await axios(
-          `/stats/browsers?projectId=${project?.id}&startDate=${dayjs().subtract(
-            6,
-            "days"
-          )}&endDate=${dayjs()}&take=${take}`
-        );
+        const { data } = await axios(`${url}browser`);
         setBrowsers(data);
+      } catch (error) {}
+    })();
+    (async () => {
+      try {
+        const { data } = await axios(`${url}country`);
+        setCountries(data);
+      } catch (error) {}
+    })();
+    (async () => {
+      try {
+        const { data } = await axios(`${url}city`);
+        setCities(data);
       } catch (error) {}
     })();
   }, [project.id]);
   return (
     <DashboardLayout pageTitle="Insights">
-      <Flex className="gap-6 flex-col lg:flex-row">
-        <Card className="max-w-lg h-[400px]">
+      <div className="grid grid-cols-2 gap-5">
+        <Card className="h-[400px]">
           <Title>Top Endpoints</Title>
           <Flex className="mt-4">
             <Text>
@@ -78,7 +84,7 @@ const Insights = () => {
           </Flex>
           <BarList data={topEndpoints} className="mt-2" />
         </Card>
-        <Card className="max-w-lg h-[400px]">
+        <Card className="h-[400px]">
           <Title>Devices</Title>
           <TabGroup>
             <TabList color="purple">
@@ -111,7 +117,40 @@ const Insights = () => {
             </TabPanels>
           </TabGroup>
         </Card>
-      </Flex>
+        <Card className="h-[400px]">
+          <Title>Location</Title>
+          <TabGroup>
+            <TabList color="purple">
+              <Tab>Countries</Tab>
+              <Tab>Cities</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Flex className="mt-4">
+                  <Text>
+                    <Bold>Country</Bold>
+                  </Text>
+                  <Text>
+                    <Bold>Requests</Bold>
+                  </Text>
+                </Flex>
+                <BarList data={countries} className="mt-2" />
+              </TabPanel>
+              <TabPanel>
+                <Flex className="mt-4">
+                  <Text>
+                    <Bold>City</Bold>
+                  </Text>
+                  <Text>
+                    <Bold>Requests</Bold>
+                  </Text>
+                </Flex>
+                <BarList data={cities} className="mt-2" />
+              </TabPanel>
+            </TabPanels>
+          </TabGroup>
+        </Card>
+      </div>
     </DashboardLayout>
   );
 };
