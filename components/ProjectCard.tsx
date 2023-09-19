c9e339943fde49f65fe763f2a4c2de133832fc26
akinloluwami <akinkunmioye42@gmail.com>
2023-09-19 09:13:09 +0100

    replaced dummy data with real data

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -11,23 +11,22 @@ interface Props {
     name: string;
     apiUrl: string;
     slug: string;
+    sparkline: number[];
+    logs: number;
+    averageRequestsPerHour: number;
   };
 }
 
 const ProjectCard: FC<Props> = ({ project }) => {
-  const { name, apiUrl, slug } = project;
+  const { name, apiUrl, slug, sparkline, logs, averageRequestsPerHour } =
+    project;
   return (
     <Link
       href={`${slug}/overview`}
       className="w-full max-w-[400px] h-[230px] border border-gray-600/70 rounded-md hover:border-purple-600 transition-colors duration-300 ease-in-out"
     >
       <div className="mb-4 w-full h-2 bg-transparent"></div>
-      <Sparklines
-        data={Array.from({ length: 48 }, () => faker.datatype.number())}
-        width={400}
-        height={50}
-        margin={5}
-      >
+      <Sparklines data={sparkline} width={400} height={50} margin={5}>
         <SparklinesLine
           style={{
             fill: "none",
@@ -38,14 +37,18 @@ const ProjectCard: FC<Props> = ({ project }) => {
       </Sparklines>
       <div className="px-6 w-full">
         <div className="w-full flex items-end justify-end">
-          <Text>2 requests/h</Text>
+          <Text>
+            {averageRequestsPerHour !== 0 && averageRequestsPerHour < 1
+              ? averageRequestsPerHour.toFixed(2)
+              : averageRequestsPerHour}{" "}
+            requests/h
+          </Text>
         </div>
         <div className="mt-3">
           <Title className="font-semibold">{name}</Title>
           <Text className="text-sm text-gray-500">{apiUrl}</Text>
           <div className="flex items-center mt-7">
-            <TbLayoutList className="mr-1" />{" "}
-            {millify(faker.number.int({ min: 1, max: 50000 }))} requests
+            <TbLayoutList className="mr-1" /> {millify(logs)} requests
           </div>
         </div>
       </div>
