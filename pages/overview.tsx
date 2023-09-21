fd87e9e4d581c687ba3fcf9cb8dac9ffcf5641d0
akinloluwami <akinkunmioye42@gmail.com>
2023-09-21 13:11:06 +0100

    updated page content

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -1,10 +1,15 @@
+import AddNewProject from "@/components/AddNewProject";
+import Logo from "@/components/Logo";
 import ProjectCard from "@/components/ProjectCard";
 import { axios } from "@/configs/axios";
-import { Button, Title } from "@tremor/react";
+import { Button, Title, Text } from "@tremor/react";
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { CgSpinner } from "react-icons/cg";
+import { MdOutlineRocketLaunch } from "react-icons/md";
 
 const Overview = () => {
   const [projects, setProjects] = useState<
@@ -19,10 +24,13 @@ const Overview = () => {
     }[]
   >([]);
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     (async () => {
       const { data } = await axios("/project");
       setProjects(data);
+      setLoading(false);
     })();
   }, []);
 
@@ -44,27 +52,55 @@ const Overview = () => {
       console.log(error);
     }
   };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className="flex items-center  justify-center flex-col py-10">
+    <div className="flex items-center justify-center flex-col py-5">
+      <AddNewProject
+        isOpen={isModalOpen}
+        closeModal={() => setIsModalOpen(false)}
+      />
       <Head>
         <title>Overview • LogDrop</title>
       </Head>
       <div className="flex items-center justify-between w-full lg:px-14 px-5">
-        <Title>Welcome back</Title>
+        <Logo />
         <Button color="red" variant="light" onClick={logout}>
           Log out
         </Button>
       </div>
-      <div className="mt-20 w-full">
-        <h2 className="text-3xl font-semibold text-center">
-          Choose your fighter
-        </h2>
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 mt-10 gap-5 lg:w-[90%] mx-auto px-5">
-          {projects.map((project) => (
-            <ProjectCard project={project} key={project.id} />
-          ))}
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <CgSpinner size={30} className="animate-spin mt-40" />
         </div>
-      </div>
+      ) : (
+        <div className="mt-20 w-full">
+          {projects.length === 0 ? (
+            <div className="flex items-center justify-center flex-col">
+              <Title className="!lg:text-5xl !text-4xl text-center">
+                Welcome to LogDrop
+              </Title>
+              <Text className="!text-lg !font-semibold mt-1">
+                Let's create your first project.
+              </Text>
+              <button
+                className="bg-white text-black font-semibold px-4 py-3 rounded-md flex items-center gap-2 hover:shadow-md transition-all duration-300 ease-in-out hover:shadow-purple-500 mt-10"
+                onClick={() => setIsModalOpen(true)}
+              >
+                <MdOutlineRocketLaunch size={20} />
+                Create your first project
+              </button>
+            </div>
+          ) : (
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 mt-10 gap-5 lg:w-[90%] mx-auto px-5">
+              {projects.map((project) => (
+                <ProjectCard project={project} key={project.id} />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
       {/* <small className="mt-10 text-center text-gray-600">
         This is a temporary page. I'm currently working on something!
       </small> */}
