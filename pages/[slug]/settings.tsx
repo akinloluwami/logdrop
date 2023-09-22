c240031c9cc6c61749e71a8dc568deb0b1c9a0c0
akinloluwami <akinkunmioye42@gmail.com>
2023-09-22 16:25:12 +0100

    trigger checkout

--- pages/[slug]/settings.tsx
+++ pages/[slug]/settings.tsx
@@ -1,5 +1,6 @@
 import Cooking from "@/components/Cooking";
 import General from "@/components/Settings/General";
+import PaddleLoader from "@/components/paddleLoader";
 import DashboardLayout from "@/layouts/DashboardLayout";
 import {
   ProgressBar,
@@ -17,6 +18,7 @@ import {
 const Settings = () => {
   return (
     <DashboardLayout pageTitle="Settings">
+      <PaddleLoader />
       <TabGroup>
         <TabList className="!bg-transparent border-gray-800" variant="solid">
           <Tab className="!bg-black !text-white">General</Tab>
@@ -29,7 +31,28 @@ const Settings = () => {
             </div>
           </TabPanel>
           <TabPanel>
-            <Cooking />
+            <button
+              className="mt-10 bg-white shadow-md rounded-md border border-gray-800 py-2 px-4 text-sm font-medium text-gray-700 hover:bg-
+              gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={() => {
+                //@ts-ignore
+                Paddle.Checkout.open({
+                  settings: {
+                    displayMode: "overlay",
+                    theme: "dark",
+                    locale: "en",
+                  },
+                  items: [
+                    {
+                      priceId: "pri_01hawzr8pc4fh41fwz7ep1enfc",
+                      quantity: 1,
+                    },
+                  ],
+                });
+              }}
+            >
+              Upgrade to Pro.
+            </button>
           </TabPanel>
         </TabPanels>
       </TabGroup>
