a1d46cda32a72afb0459e4fc15bd2eea4118c4c4
akinloluwami <akinkunmioye42@gmail.com>
2023-10-15 09:25:31 +0100

    feat: updated column state in db

--- pages/api/project/[id]/reports.ts
+++ pages/api/project/[id]/reports.ts
@@ -7,9 +7,17 @@ import dayjs from "dayjs";
 
 const handler = async (req: CustomRequest, res: NextApiResponse) => {
   try {
-    const interval = req.query.interval.toLowerCase() as "weekly" | "monthly";
+    const interval = String(req.query.interval).toLowerCase();
+    const projectId = Number(req.query.id);
 
-    if (interval) {
+    if (!projectId) {
+      res.status(400).json({
+        message: "Project ID is required",
+      });
+      return;
+    }
+
+    if (!interval) {
       res.status(400).json({
         message: "",
       });
@@ -17,10 +25,41 @@ const handler = async (req: CustomRequest, res: NextApiResponse) => {
       return;
     }
 
-    if (interval !== "weekly" || interval !== "monthly") {
+    if (interval !== "weekly" && interval !== "monthly") {
       return res.status(400).json({
         message: "Invalid interval",
       });
     }
-  } catch (error) {}
+
+    const project = await prisma.project.findFirst({
+      where: {
+        id: projectId,
+      },
+    });
+
+    if (interval === "weekly") {
+      await prisma.project.update({
+        where: {
+          id: projectId,
+        },
+        data: {
+          sendWeeklyReports: !project?.sendWeeklyReports,
+        },
+      });
+    }
+
+    res.status(200).json({
+      message: "Successfully updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong.",
+    });
+  }
 };
+
+export default requestMethod(["PATCH"])(
+  (req: CustomRequest, res: NextApiResponse) => {
+    authenticateToken(req, res, () => handler(req, res));
+  }
+);
