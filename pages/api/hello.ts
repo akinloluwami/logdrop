38c0331f83d0a8265f13667339d45ffd0421ad1b
akinloluwami <akinkunmioye42@gmail.com>
2023-11-02 10:07:18 +0100

    .

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,26 +1,9 @@
-import { CustomRequest } from "@/interfaces";
-import authenticateToken from "@/middlewares/auth";
-import { requestMethod } from "@/middlewares/requestMethod";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const result = "";
-
-    if (!result) {
-      res.status(400).send({ error: "failed to fetch data" });
-      return;
-    }
-
-    res.status(200).send({ result });
-  } catch (err) {
-    res.status(500).send({ error: "failed to fetch data" });
-  }
+  return res.json({
+    message: "Hello World!",
+  });
 }
 
-export default requestMethod(["GET"])((
-  req: CustomRequest,
-  res: NextApiResponse
-) => {
-  authenticateToken(req, res, () => handler(req, res));
-});
+export default handler;
