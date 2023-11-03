23501f8284ed58ce60028c4b010fef1e848ca424
akinloluwami <akinkunmioye42@gmail.com>
2023-11-03 08:55:13 +0100

    node-mocks-http

--- __tests__/log.test.ts
+++ __tests__/log.test.ts
@@ -1,10 +1,17 @@
-import request from "supertest";
-import app from "@/pages/api/hello";
+import hello from "@/pages/api/hello";
+import { createMocks } from "node-mocks-http";
 
-describe("Test", () => {
-  it("test", async () => {
-    const response = await request(app).get("/hello");
-    expect(response.status).toBe(200);
-    expect(response.text).toBe("ok");
-  }, 10000);
+describe("API Endpoint Tests", () => {
+  it("should return hello world message", async () => {
+    const { req, res } = createMocks({
+      method: "GET",
+    });
+
+    hello(req, res);
+
+    const data = JSON.parse(res._getData());
+
+    expect(res.statusCode).toBe(200);
+    expect(data.message).toBe("Hello World!");
+  });
 });
