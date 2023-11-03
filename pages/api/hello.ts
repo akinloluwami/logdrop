4f201c53d74c5b31e7900a7233cd79c2d743c94d
akinloluwami <akinkunmioye42@gmail.com>
2023-11-03 08:55:24 +0100

    _

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   return res.json({
     message: "Hello World!",
   });
 }
-
-export default handler;
