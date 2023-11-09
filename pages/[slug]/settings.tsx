41d24422771b38835d14e386d0b89f0e188102d9
akinloluwami <akinkunmioye42@gmail.com>
2023-11-09 02:54:07 +0100

    feat: select default tab

--- pages/[slug]/settings.tsx
+++ pages/[slug]/settings.tsx
@@ -1,26 +1,35 @@
-import Cooking from "@/components/Cooking";
 import General from "@/components/Settings/General";
 import PlanAndUsage from "@/components/Settings/PlanAndUsage";
-import PaddleLoader from "@/components/paddleLoader";
 import DashboardLayout from "@/layouts/DashboardLayout";
-import {
-  ProgressBar,
-  Card,
-  Flex,
-  Text,
-  Metric,
-  TabList,
-  Tab,
-  TabGroup,
-  TabPanels,
-  TabPanel,
-} from "@tremor/react";
+import { TabList, Tab, TabGroup, TabPanels, TabPanel } from "@tremor/react";
+import { useRouter } from "next/router";
+import useHref from "use-href";
 
 const Settings = () => {
+  const tabs = [{ index: 1, title: "plan-and-usage" }];
+
+  const router = useRouter();
+
+  const { addQueryParam, deleteQueryParam } = useHref();
+
   return (
     <DashboardLayout pageTitle="Settings">
-      <PaddleLoader />
-      <TabGroup>
+      <TabGroup
+        defaultIndex={
+          tabs.find((tab) => tab.title === router.query.tab)?.index || 0
+        }
+        onIndexChange={(index) => {
+          if (index === 0) {
+            deleteQueryParam("tab");
+            return;
+          }
+
+          addQueryParam(
+            "tab",
+            tabs.find((tabs) => tabs.index === index)?.title!
+          );
+        }}
+      >
         <TabList className="!bg-transparent border-gray-800" variant="solid">
           <Tab className="!bg-black !text-white">General</Tab>
           <Tab className="!bg-black !text-white">Plan & Usage</Tab>
