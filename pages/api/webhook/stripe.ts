d989c72703627f98de2d70174d571fb13374209b
akinloluwami <akinkunmioye42@gmail.com>
2023-11-10 14:36:23 +0100

    .

--- pages/api/webhook/stripe.ts
+++ pages/api/webhook/stripe.ts
@@ -4,27 +4,87 @@ import { prisma } from "@/prisma";
 import { Stripe } from "stripe";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const {
-    event_type,
-    data,
-  }: {
-    event_type: string;
-    data: {
-      items: any;
-      id: string;
-      customer_id: string;
-    };
-  } = req.body;
+  // const {
+  //   event_type,
+  //   data,
+  // }: {
+  //   event_type: string;
+  //   data: {
+  //     items: any;
+  //     id: string;
+  //     customer_id: string;
+  //   };
+  // } = req.body;
 
   const tiers = [
     {
-      id: "pro_01hawzmmjgvtg53pzk6ha9mw94",
+      sub_id: "sub_1OAqx8CqizJngwvWFVW5A4A9",
       quota: 15000,
     },
+    {
+      sub_id: "",
+      quota: 50000,
+    },
+    {
+      sub_id: "",
+      quota: 100000,
+    },
+    {
+      sub_id: "",
+      quota: 150000,
+    },
+    {
+      sub_id: "",
+      quota: 250000,
+    },
+    {
+      sub_id: "",
+      quota: 650000,
+    },
+    {
+      sub_id: "",
+      quota: 1300000,
+    },
+    {
+      sub_id: "",
+      quota: 2300000,
+    },
+    {
+      sub_id: "",
+      quota: 3500000,
+    },
+    {
+      sub_id: "",
+      quota: 5000000,
+    },
+    {
+      sub_id: "",
+      quota: 8000000,
+    },
+    {
+      sub_id: "",
+      quota: 10000000,
+    },
   ];
-  console.log(event_type);
+  // console.log(event_type);
 
-  const stripe = new Stripe(process.env.STRIPE_API_KEY!);
+  new Stripe(process.env.STRIPE_API_KEY!);
+
+  const event = req.body as Stripe.Event;
+
+  const checkout = event.data.object as Stripe.Checkout.Session;
+
+  // if (!checkout.customer) {
+  //   return res.status(400).send("Missing item in webhook callback.");
+  // }
+
+  const stripeId = String(checkout.customer);
+
+  console.log("====================================");
+  console.log(event);
+  console.log("====================================");
+
+  res.status(200).send("ok");
 };
 
 export default requestMethod(["POST"])(handler);
