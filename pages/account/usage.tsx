746b65f09a11a90d0824978c88ab9146eac3e46d
akinloluwami <akinkunmioye42@gmail.com>
2023-11-12 05:47:31 +0100

    .

--- pages/account/usage.tsx
+++ pages/account/usage.tsx
@@ -1,7 +1,21 @@
+import { axios } from "@/configs/axios";
 import AccountLayout from "@/layouts/AccountLayout";
-import React from "react";
+import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
 
 const Usage = () => {
+  const [start, setStart] = useState(
+    dayjs().startOf("month").subtract(1, "month").format("YYYY-MM-DD")
+  );
+  const [end, setEnd] = useState(dayjs().format("YYYY-MM-DD"));
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(`/me/usage?from=${start}&to=${end}`);
+      console.log(res);
+    })();
+  }, []);
+
   return (
     <AccountLayout>
       <div className=""></div>
