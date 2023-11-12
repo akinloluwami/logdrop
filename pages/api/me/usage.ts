d994022bd30d99b1f6e38374322e8043c5df3c7b
akinloluwami <akinkunmioye42@gmail.com>
2023-11-12 05:23:37 +0100

    usage

--- pages/api/me/usage.ts
+++ pages/api/me/usage.ts
@@ -2,21 +2,55 @@ import { CustomRequest } from "@/interfaces";
 import authenticateToken from "@/middlewares/auth";
 import { requestMethod } from "@/middlewares/requestMethod";
 import { prisma } from "@/prisma";
+import dayjs from "dayjs";
 import { NextApiResponse } from "next";
 
 const handler = async (req: CustomRequest, res: NextApiResponse) => {
-  if (req.method === "GET") {
-    const usage = await prisma.log.count({
-      where: {
-        project: {
+  try {
+    if (req.method === "GET") {
+      const start = dayjs().startOf("month");
+      const end = dayjs();
+
+      const usage = await prisma.log.count({
+        where: {
+          project: {
+            userId: req.user?.id,
+          },
+          createdAt: {
+            gte: start.toDate(),
+            lte: end.toDate(),
+          },
+        },
+      });
+
+      const usageByProject = await prisma.project.findMany({
+        where: {
           userId: req.user?.id,
         },
-      },
-    });
-    res.status(200).json({ usage });
+        select: {
+          name: true,
+          _count: {
+            select: {
+              logs: {
+                where: {
+                  createdAt: {
+                    gte: start.toDate(),
+                    lte: end.toDate(),
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+
+      res.status(200).json({ usage, usageByProject });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
   }
 };
-
 export default requestMethod(["GET", "POST"])((
   req: CustomRequest,
   res: NextApiResponse
