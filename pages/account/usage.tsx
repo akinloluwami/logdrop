ac056e7b78f164752509b500e2d4367560f68e28
akinloluwami <akinkunmioye42@gmail.com>
2023-11-12 13:55:39 +0100

    rendered charts and stuff

--- pages/account/usage.tsx
+++ pages/account/usage.tsx
@@ -1,24 +1,85 @@
 import { axios } from "@/configs/axios";
 import AccountLayout from "@/layouts/AccountLayout";
 import dayjs from "dayjs";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Card, DonutChart, ProgressCircle, Title } from "@tremor/react";
 
 const Usage = () => {
   const [start, setStart] = useState(
-    dayjs().startOf("month").subtract(1, "month").format("YYYY-MM-DD")
+    dayjs().startOf("month").format("YYYY-MM-DD")
   );
   const [end, setEnd] = useState(dayjs().format("YYYY-MM-DD"));
 
+  const [usage, setUsage] = useState<{
+    percentage: number;
+    usage: number;
+    quota: number;
+    usageByProject: {
+      name: string;
+      logs: number;
+    }[];
+  } | null>(null);
+
   useEffect(() => {
     (async () => {
-      const res = await axios.get(`/me/usage?from=${start}&to=${end}`);
-      console.log(res);
+      const { data } = await axios.get(`/me/usage?from=${start}&to=${end}`);
+      setUsage(data);
     })();
   }, []);
 
   return (
     <AccountLayout>
-      <div className=""></div>
+      <div className="flex gap-5">
+        <Card className="flex flex-col">
+          <Title className="text-center mb-5 text-xl">Requests</Title>
+          <ProgressCircle
+            value={usage?.percentage}
+            radius={80}
+            strokeWidth={15}
+            tooltip={`Requests: ${usage?.usage}, Quota: ${usage?.quota}`}
+            size="lg"
+            color="purple"
+          >
+            <span>{usage?.percentage}%</span>
+          </ProgressCircle>
+
+          <div className="mt-10">
+            <Title className="text-center mb-5 text-xl">
+              Requests by projects
+            </Title>
+            <DonutChart
+              data={usage?.usageByProject!}
+              index="name"
+              category="logs"
+              colors={[
+                "purple",
+                "blue",
+                "amber",
+                "cyan",
+                "emerald",
+                "fuchsia",
+                "gray",
+                "green",
+              ]}
+              variant="pie"
+              valueFormatter={(value) => `${value} requests`}
+            />
+          </div>
+        </Card>
+        {/* <Card className="flex flex-col">
+          <Title className="text-center mb-5 text-xl">Events</Title>
+          <ProgressCircle
+            value={usage?.percentage}
+            radius={80}
+            strokeWidth={15}
+            tooltip={`Requests: ${usage?.usage}, Quota: ${usage?.quota}`}
+            size="lg"
+            color="purple"
+          >
+            <span>{usage?.percentage}%</span>
+          </ProgressCircle>
+        </Card> */}
+      </div>
     </AccountLayout>
   );
 };
