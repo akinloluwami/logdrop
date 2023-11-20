e39dcd205957c0d8c72852082f6ed770276f60da
akinloluwami <akinkunmioye42@gmail.com>
2023-11-20 04:49:15 +0100

    get new data

--- pages/api/cron/reports/weekly.ts
+++ pages/api/cron/reports/weekly.ts
@@ -7,21 +7,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     where: { sendWeeklyReports: true },
   });
 
+  if (!projects) return res.status(404).json({ message: "No projects found" });
+
+  const createdAt = {
+    gte: dayjs().subtract(7, "day").toDate(),
+  };
+
   for (const project of projects) {
     const totalLogs = await prisma.log.count({
       where: {
         projectId: project.id,
-        createdAt: {
-          gte: dayjs().subtract(7, "day").toDate(),
-        },
+        createdAt,
       },
     });
     const topEndpoints = await prisma.log.groupBy({
       by: ["endpoint"],
       where: {
-        createdAt: {
-          gte: dayjs().subtract(7, "day").toDate(),
-        },
+        createdAt,
       },
       _count: {
         endpoint: true,
@@ -39,7 +41,37 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       logs: endpoint._count.endpoint,
     }));
 
-    const data = { totalLogs, topEndpointLogs };
+    const averageResponseTime = await prisma.log.aggregate({
+      _avg: { elapsedDuration: true },
+      where: { createdAt },
+    });
+
+    const requestsByMethod = await prisma.log.groupBy({
+      by: ["method"],
+      where: {
+        createdAt,
+      },
+      _count: {
+        method: true,
+      },
+      orderBy: {
+        _count: {
+          method: "desc",
+        },
+      },
+      take: 10,
+    });
+
+    const data = {
+      totalLogs,
+      averageResponseTime:
+        averageResponseTime._avg.elapsedDuration?.toFixed(0) + "ms",
+      topEndpointLogs,
+      requestsByMethod: requestsByMethod.map((method) => ({
+        method: method.method,
+        count: method._count.method,
+      })),
+    };
     //send email to project owner
     console.log(data);
   }
